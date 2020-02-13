18d47e3a4329fbcd5a1087e924abc1debdb94472
archlicher <christophergbbg@yahoo.com>
2020-02-13 11:35:06 +0200

    Add some minor improvements to the specs

--- spec/draw_spec.rb
+++ spec/draw_spec.rb
@@ -2,9 +2,11 @@ require '~/mentormate/lib/draw'
 
 RSpec.describe Draw do
 	describe '#draw' do
-		it { expect(Draw.new.draw(3)).to be_nil }
-		it { expect(Draw.new.draw(5)).to be_nil }
-		it { expect(Draw.new.draw(7)).to be_nil }
+		let(:d) { Draw.new }
+
+		it { expect(d.draw(3)).to be_nil }
+		it { expect(d.draw(5)).to be_nil }
+		it { expect(d.draw(7)).to be_nil }
 		# it { expect(Draw.new.draw(9999)).to be_nil }
 	end
 end
--- spec/validations_spec.rb
+++ spec/validations_spec.rb
@@ -2,20 +2,22 @@ require '~/mentormate/lib/validation'
 
 RSpec.describe Validations do
 	describe '#validate' do
+		let(:v) { Validations.new }
+
 		context 'passes only valid values' do
-			it { expect(Validations.new.validate(3)).to eq(3) }
-			it { expect(Validations.new.validate(5)).to eq(5) }
-			it { expect(Validations.new.validate(7)).to eq(7) }
-			it { expect(Validations.new.validate(9999)).to eq(9999) }
+			it { expect(v.validate(3)).to eq(3) }
+			it { expect(v.validate(5)).to eq(5) }
+			it { expect(v.validate(7)).to eq(7) }
+			it { expect(v.validate(9999)).to eq(9999) }
 		end
 
-		context 'when an invalid value is passed' do
-			it { expect(Validations.new.validate(0)).to eq('Your input did not meet the criteria. Please try again.') }
-			it { expect(Validations.new.validate(1)).to eq('Your input did not meet the criteria. Please try again.') }
-			it { expect(Validations.new.validate(2)).to eq('Your input did not meet the criteria. Please try again.') }
-			it { expect(Validations.new.validate(4)).to eq('Your input did not meet the criteria. Please try again.') }
-			it { expect(Validations.new.validate(9998)).to eq('Your input did not meet the criteria. Please try again.') }
-			it { expect(Validations.new.validate(10_001)).to eq('Your input did not meet the criteria. Please try again.') }
+		context 'when an invalid value' do
+			it { expect(v.validate(0)).to eq('Your input did not meet the criteria. Please try again.') }
+			it { expect(v.validate(1)).to eq('Your input did not meet the criteria. Please try again.') }
+			it { expect(v.validate(2)).to eq('Your input did not meet the criteria. Please try again.') }
+			it { expect(v.validate(4)).to eq('Your input did not meet the criteria. Please try again.') }
+			it { expect(v.validate(9998)).to eq('Your input did not meet the criteria. Please try again.') }
+			it { expect(v.validate(10_001)).to eq('Your input did not meet the criteria. Please try again.') }
 		end
 	end
 end
