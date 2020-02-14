0e1f4fed7fb78e737e619ed37f4cc0f321b6c349
archlicher <christophergbbg@yahoo.com>
2020-02-14 09:35:36 +0200

    [MM-8] Decoupling validations and tests

--- lib/validations.rb
+++ lib/validations.rb
@@ -1,10 +1,24 @@
 
 class Validations
 	def validate(n)
-		unless n.is_a?(Integer) && n > 2 && n < 10_000 && n.odd?
+		unless integer?(n) &&
+			   between_2_and_10_000?(n) &&
+			   odd?(n)
 			puts 'Your input did not meet the criteria. Please try again.'
 		else
 			n
 		end
+	end
+
+	def integer?(n)
+		true if n.is_a? Integer
+	end
+
+	def between_2_and_10_000?(n)
+		true if 2 < n && n < 10_000
+	end
+
+	def odd?(n)
+		true if n.odd?
 	end
 end
--- spec/validations_spec.rb
+++ spec/validations_spec.rb
@@ -1,9 +1,9 @@
 require '~/mentormate/lib/validations'
 
 RSpec.describe Validations do
-	describe '#validate' do
-		let(:v) { Validations.new }
+	let(:v) { Validations.new }
 
+	describe '#validate' do
 		context 'passes only valid values' do
 			it { expect(v.validate(3)).to eq(3) }
 			it { expect(v.validate(5)).to eq(5) }
@@ -18,6 +18,52 @@ RSpec.describe Validations do
 			it { expect(v.validate(4)).to be_nil }
 			it { expect(v.validate(9998)).to be_nil }
 			it { expect(v.validate(10_001)).to be_nil }
+		end
+	end
+
+	describe '#integer?' do
+		context 'valid' do
+			it { expect(v.integer?(0)).to be_truthy }
+			it { expect(v.integer?(2)).to be_truthy }
+			it { expect(v.integer?(1000)).to be_truthy }
+			it { expect(v.integer?(-123)).to be_truthy }
+		end
+
+		context 'invalid' do
+			it { expect(v.integer?(0.1)).to be_falsy }
+			it { expect(v.integer?('x')).to be_falsy }
+			it { expect(v.integer?('exit')).to be_falsy }
+			it { expect(v.integer?(-2.3)).to be_falsy }
+		end
+	end
+
+	describe '#between_2_and_10_000?' do
+		context 'yes' do
+			it { expect(v.between_2_and_10_000?(2.1)).to be_truthy }
+			it { expect(v.between_2_and_10_000?(3)).to be_truthy }
+			it { expect(v.between_2_and_10_000?(4.4)).to be_truthy }
+			it { expect(v.between_2_and_10_000?(9999)).to be_truthy }
+			it { expect(v.between_2_and_10_000?(9999.9)).to be_truthy }
+		end
+
+		context 'no' do
+			it { expect(v.between_2_and_10_000?(1)).to be_falsy }
+			it { expect(v.between_2_and_10_000?(10_000)).to be_falsy }
+		end
+	end
+
+	describe '#odd?' do
+		context 'yes' do
+			it { expect(v.odd?(1)).to be_truthy }
+			it { expect(v.odd?(-1)).to be_truthy }
+			it { expect(v.odd?(3)).to be_truthy }
+			it { expect(v.odd?(9999)).to be_truthy }
+		end
+
+		context 'no' do
+			it { expect(v.odd?(0)).to be_falsy }
+			it { expect(v.odd?(2)).to be_falsy }
+			it { expect(v.odd?(-2)).to be_falsy }
 		end
 	end
 end
